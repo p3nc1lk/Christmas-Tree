0ddc7a1153aba760988a5fe1e53f161e22ecb161
Ethin DalPian <53026956+p3nc1lk@users.noreply.github.com>
2024-11-21 16:15:16 -0500

    Update main.py
    STOP MAKING ME COMMIT

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ import random
 import math
 import sys
 ledcount = 100
-leds = neopixel.NeoPixel(board., ledCount, pixel_order=neopixel.RGB, auto_write=False)
-#Note to self -> add which pin you are using after board. EX: board.D18 for pin 18
+leds = neopixel.NeoPixel(board.D18, ledcount, pixel_order=neopixel.RGB, auto_write=False)
+#Note to self -> add which pin you are using on board. EX: board.D18 for pin 18
+ORDER = neopixel.RGB
+
+
+
+
+
